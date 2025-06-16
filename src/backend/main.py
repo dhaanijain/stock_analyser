bcb9fa207ef9635b481f2ad880fdaeb636c63a74
Dhaani <dhaanijain@gmail.com>
2025-06-16 11:40:19 +0530

    changes in main

--- src/backend/main.py
+++ src/backend/main.py
@@ -3,6 +3,7 @@ from data_handeling import all_articles_fetch_data, stock_price_fetch_data
 from data_handeling import all_articles_push_data, stock_price_push_data
 from data_processing import sentiment_analysis
 from data_processing import merge_tables
+from data_handeling import stock_price_fetch_data_if_not_exists
 import time
 
 
@@ -11,8 +12,9 @@ def main():
     start_date = "2025-05-12"
     end_date = "2025-06-11"
     
-    # df_price = stock_price_fetch_data(ticker, start_date, end_date)
-    # stock_price_push_data(df_price)
+    df_price = stock_price_fetch_data(ticker, start_date, end_date)
+    stock_price_push_data(df_price)
+    stock_price_fetch_data_if_not_exists(ticker, start_date, end_date)
 
     
     # df_articles = all_articles_fetch_data(ticker, start_date, end_date)
@@ -22,9 +24,9 @@ def main():
     
     # sentiment_analysis(ticker, start_date, end_date)
     
-    df_merged = merge_tables(ticker, start_date, end_date) #includes call to groupby_all_articles
-    print(df_merged)
-    print("run successfully")
+    # df_merged = merge_tables(ticker, start_date, end_date) #includes call to groupby_all_articles
+    # print(df_merged)
+    # print("run successfully")
     
     
     
